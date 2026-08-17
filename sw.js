45cf2fe04eda471dad155dfa13f9449d186b4c02
boteandopiola <85791254+boteandopiola@users.noreply.github.com>
2026-08-17 02:28:50 -0300

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,9 @@
-const CACHE_NAME = "youtube-pwa-v2";
-
-const FILES = [
-    "./",
-    "./index.html",
-    "./app.js",
-    "./manifest.json",
-    "./icon-192.png",
-    "./icon-512.png"
-];
+const CACHE_NAME = "mytube-v1";
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(FILES);
-        })
-    );
-
     self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            );
-        })
-    );
-
-    self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(cachedResponse => {
-            return cachedResponse || fetch(event.request);
-        })
-    );
+    event.waitUntil(self.clients.claim());
 });
